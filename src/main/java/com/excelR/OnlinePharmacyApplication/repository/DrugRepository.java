51c5c98fbb903fbd046b1a00c148ab0de39abb63
Shubham Ghaytadkar <shubhamghaytadkar177@gmail.com>
2025-07-28 17:35:37 +0530

    Delete src directory

--- src/main/java/com/excelR/OnlinePharmacyApplication/repository/DrugRepository.java
+++
@@ -1,8 +0,0 @@
-package com.excelR.OnlinePharmacyApplication.repository;
-import com.excelR.OnlinePharmacyApplication.entity.Drug;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DrugRepository extends JpaRepository<Drug, Long> {
-    Optional<Drug> findByName(String name);
-}
--- src/main/java/com/excelR/OnlinePharmacyApplication/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.excelR.OnlinePharmacyApplication.repository;
-import com.excelR.OnlinePharmacyApplication.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserId(Long userId);
-}
