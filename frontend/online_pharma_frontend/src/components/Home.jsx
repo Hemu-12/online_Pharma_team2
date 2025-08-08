589162e8085e990962efe0a85fe9c9842295963b
Shradha_misal <121875579+shinobusin@users.noreply.github.com>
2025-08-08 11:33:09 +0530

    Update Home.jsx
    admin login

--- frontend/online_pharma_frontend/src/components/Home.jsx
+++ frontend/online_pharma_frontend/src/components/Home.jsx
@@ -52,6 +52,13 @@ const Navbar = ({ toggleDarkMode }) => {
           <FaShoppingCart className="icon" />
         </Link>
 
+        {*/admin login*/}
+        <Link to="/admin-login" className="sign-in-btn">
+          <FaUser className="icon" />
+          <span>Sign In</span>
+        </Link>
+
+
         <button className="sign-in-btn">
           <FaUser className="icon" />
           <span>Sign In</span>
